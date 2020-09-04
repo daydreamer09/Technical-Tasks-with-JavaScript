db501041e8af5b0ed06797667c70008b8c001354
daydreamer09 <52257343+daydreamer09@users.noreply.github.com>
2020-09-04 16:23:28 +0100

    Update Task_6.js

--- Task_6.js
+++ Task_6.js
@@ -1,6 +1,6 @@
 // declaring variables
 let firstArray = [8,9,2,1,4,5,6];
-let sum = 15;
+let sum = 19;
 
 // bubble sort method for firstArray
 for (var y = 0; y< firstArray.length; y++){
@@ -18,31 +18,33 @@ function ArrayAndSum(array, sum){
     //declaring variables
     let done = false;
     let result =[];
-    let firstElement = array[0],
-        secondElement,
-        lastElement;
+    let firstElement = 0, secondElement = 0, lastElement = 0;
+    
 
     //method to determine which numbers are equal to the sum    
-    for (var i = 1; i < array.length - 1; i++){ //loopping around the array and check the values
-
-        if((array[0]+array[i]+array[i+1]) === sum) 
-            done = true;    
-
-        if (done == true){ // assigning the values to variables
-            secondElement = array[i];
-            lastElement = array[i+1]; 
-        } 
-        break;
+    for (var i = 1; i < array.length; i++){ //looping around the array and check the values
+        for(var j = i + 1; j < array.length; j++){ // looping for second value
+            for (var k = j+1; k < array.length; k++){ // looping for the third value
+                if((array[i]+array[j]+array[k]) === sum){
+                    done = true;
+                    firstElement = array[i];
+                    secondElement = array[j];
+                    lastElement = array[k];
+                    break;
+                }       
+            }
+        }
     }
+        if (done === true){ // assigning the values to variables
+            result.push(firstElement);
+            result.push (secondElement);
+            result.push(lastElement);
+        } else if(done === false){
+            result.push(-1);
+        }
+    
 
-    //pushing the values to an array to return the values as n array
-    if (done == true){
-        result.push(firstElement);
-        result.push(secondElement);
-        result.push(lastElement);
-    }else if (done == false){
-        result.push(-1);
-    }
+    
 
     return result;
 }
