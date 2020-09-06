1234e5ba0afa5b1cf4cef9dd25cb5f433377ba14
daydreamer09 <52257343+daydreamer09@users.noreply.github.com>
2020-09-06 07:57:39 +0100

    Update Task_4.js

--- Task_4.js
+++ Task_4.js
@@ -1,12 +1,22 @@
-// Incomplete code for password validator
+//Asking the user to enter password
 let password= prompt("Please input your password: "); // ask user for password
 
 function PasswordValidator(enteredPassword){
+    
+    // declaring variables with regular expressions
+    var numbers = /^[0-9]+$/, 
+        letters = /^[A-Za-z]+$/,
+        numbersAndLetters = /^[0-9a-zA-Z]+$/,
+        numberLettersAndSpecialCharacters = /^[@_!#$%^&*()<>?/|}{~:0-9a-zA-Z]*$/ ;
     //checking the type of input entered
-    if(isNaN(enteredPassword)){
+    if (enteredPassword.match(numbers)){ //checking for only numbers
         return 0;
-    }else if(isNaN(enteredPassword) == false){
+    }else if(enteredPassword.match(letters)){ //checking for only letters
         return 1;
+    }else if (enteredPassword.match( numbersAndLetters)){ //checking for numbers and letters
+        return 2;
+    }else if(enteredPassword.match(numberLettersAndSpecialCharacters)){ // checking for numbers, letters, and special characters
+        return 3;
     }
 
 }
